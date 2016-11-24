ec78e6969279477c1cbf036dfe0ac90c5bc7bc42
Abhinav Swami <abhinav.swami21@gmail.com>
2016-11-24 14:03:05 -0800

    Fix application.js import

--- photogram/app/assets/javascripts/application.js
+++ photogram/app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-= require jquery
+//@import
+//= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree 
-= require bootstrap-sprockets.
-@import
+//= require bootstrap-sprockets
+
