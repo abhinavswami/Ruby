1c18e3298a52787d496f960efc15b4343548e3e1
Abhinav Swami <abhinav.swami21@gmail.com>
2016-11-29 22:22:05 -0800

    Add actions for edit and update post

--- photogram/app/controllers/posts_controller.rb
+++ photogram/app/controllers/posts_controller.rb
@@ -13,9 +13,18 @@ class PostsController < ApplicationController
 	end
 
 	def show  
-	  @post = Post.find(params[:id])
-	end 
+	  	@post = Post.find(params[:id])
+	end
+	
+	def edit  
+  		@post = Post.find(params[:id])
+	end  
 
+	def update  
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to(post_path(@post))
+	end  
 	private
 	 
 	def post_params
