8eb1ce3303afe5bde4444c856790cf4a9072224d
Abhinav Swami <abhinav.swami21@gmail.com>
2016-12-09 10:24:54 -0800

    Fix authentication error

--- photogram/app/controllers/application_controller.rb
+++ photogram/app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 #  protect_from_forgery with: :exception
+Paperclip.options[:command_path] = "/usr/local/bin/identify"
 end
