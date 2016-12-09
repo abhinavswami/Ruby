163362592c51a08cbae313b5e52b3b7902728b83
Abhinav Swami <abhinav.swami21@gmail.com>
2016-12-09 11:31:09 -0800

    Remove code duplication

--- photogram/app/controllers/posts_controller.rb
+++ photogram/app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@posts = Post.all
 	end
@@ -8,33 +10,40 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create(post_params)
-		redirect_to posts_path
+		if @post = Post.create(post_params)
+			redirect_to posts_path
+		else
+			render :new
+		end
 	end
 
 	def show  
-	  	@post = Post.find(params[:id])
-	end
+  	end
 	
 	def edit  
-  		@post = Post.find(params[:id])
-	end  
+  	end  
 
 	def update  
-		@post = Post.find(params[:id])
-		@post.update(post_params)
-		redirect_to(post_path(@post))
+		if @post.update(post_params)
+			redirect_to(post_path(@post))
+		else
+			render :edit
+		end
 	end
 
 	def destroy  
-		@post = Post.find(params[:id])
 		@post.destroy
 		redirect_to posts_path
 	end   
-	private
-	 
+	
+	private 
 	def post_params
 		params.require(:post).permit(:image, :caption)
+	end
+
+		
+	def set_post
+		@post = Post.find(params[:id])
 	end
 
 end
